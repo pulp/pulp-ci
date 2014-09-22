f0bd9f3d5a46a65faa01ff847aa0eff33996533a
Justin Garrison <justinleegarrison@gmail.com>
2014-09-22 15:23:00 -0700

    fixed file/package dependancy :tada:

--- puppet/pulp/manifests/admin.pp
+++ puppet/pulp/manifests/admin.pp
@@ -24,7 +24,7 @@ class pulp::admin (
         command => '/usr/bin/yum -y groupinstall pulp-admin',
         unless  => '/usr/bin/yum grouplist "Pulp Admin" | /bin/grep "^Installed groups"',
         timeout => 600,
-    }
+    } ->
     file { '/etc/pulp/admin/admin.conf':
         ensure  => present,
         content => template('pulp/admin.conf.erb'),
