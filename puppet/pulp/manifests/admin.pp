082477d1b0d11c97d9ccd8f42203c0b26b87fcdb
Justin Garrison <justinleegarrison@gmail.com>
2014-09-22 13:07:12 -0700

    added admin.conf.erb and removed pulp::admin::params. Updated defaults vars

--- puppet/pulp/manifests/admin.pp
+++ puppet/pulp/manifests/admin.pp
@@ -1,6 +1,25 @@
 # Call this class to install the pulp-admin client
 
-class pulp::admin inherits pulp::admin::params {
+class pulp::admin (
+    $pulp_server                = $::pulp_server,
+    $pulp_port                  = 443,
+    $pulp_api_prefix            = '/pulp/api',
+    $pulp_rsa_pub               = '/etc/pki/pub/admin/server/rsa_pub.key',
+    $verify_ssl                 = 'true',
+    $ca_path                    = '/etc/pki/tls/certs/ca-bundle.crt',
+    $upload_chunk_size          = '1048576',
+    $client_role                = 'admin',
+    $extensions_dir             = '/usr/lib/pulp/admin/extensions',
+    $id_cert_dir                = '~/.pulp/',
+    $id_cert_filename           = 'user-cert.pem',
+    $upload_working_dir         = '~/.pulp/uploads',
+    $log_filename               = '~/.pulp/admin.log',
+    $call_log_filename          = undef, #Default = '~/.pulp/server_calls.log'
+    $poll_frequency             = '1',
+    $color_output               = 'true',
+    $wrap_terminal              = 'false',
+    $wrap_width                 = 80,
+) inherits pulp::globals {
     exec { 'yum install pulp-admin':
         command => '/usr/bin/yum -y groupinstall pulp-admin',
         unless  => '/usr/bin/yum grouplist "Pulp Admin" | /bin/grep "^Installed groups"',
