5803473cc07ff6df211edf750f2d1aa37040e40f
Ilja Bobkevic <ilja.bobkevic@unibet.com>
2014-11-04 22:53:04 +0100

    Add centos support. Update repo url to https

--- puppet/pulp/manifests/globals.pp
+++ puppet/pulp/manifests/globals.pp
@@ -3,9 +3,10 @@
 class pulp::globals (
     $repo_name      = 'pulp-stable',
     $repo_descr     = 'Pulp Stable Repository',
-    $repo_baseurl   = 'http://repos.fedorapeople.org/repos/pulp/pulp/stable/2',
+    $repo_baseurl   = 'https://repos.fedorapeople.org/repos/pulp/pulp/stable/2',
     $repo_enabled   = '1',
-    $repo_gpgcheck  = '0'
+    $repo_gpgcheck  = '0',
+    $repo_priority  = 0
 ) {
     # Install the repository file
     case $::operatingsystem {
@@ -36,8 +37,21 @@ class pulp::globals (
                 gpgcheck => $repo_gpgcheck
             }
         }
+        'CentOS': {
+            yumrepo { 'Pulp repo':
+                name     => $repo_name,
+                descr    => $repo_descr,
+                baseurl  => "${repo_baseurl}/${::operatingsystemmajrelease}Server/${::architecture}/",
+                enabled  => $repo_enabled,
+                gpgcheck => $repo_gpgcheck
+            }
+            # For compatibility with yum module
+            if $priority != 0 {
+              Yumrepo['Pulp repo'] { priority => $repo_priority }
+            }
+        }
         default: {
-            fail('Currently, this module only supports RedHat and Fedora operating systems.')
+            fail('Currently, this module only supports RedHat, CentOS and Fedora operating systems.')
         }
     }
 }
