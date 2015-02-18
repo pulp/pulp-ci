007ddc0cc4d41d28f96ee2208dff07469e2290aa
Barnaby Court <bcourt@redhat.com>
2015-02-18 18:14:40 -0500

    Add github to the known hosts

--- ci/deploy/utils/puppet/jenkins_node_setup.pp
+++ ci/deploy/utils/puppet/jenkins_node_setup.pp
@@ -9,6 +9,13 @@ class jenkins_node_setup(){
     type => 'ssh-rsa',
     key  => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC6DJ8fmd61DWPCMiOEuy96ajI7rL3rWu7C9NQhE9a4SfyaiBcghREHJNCz9LGJ57jtOmNV0+UEDhyvTckZI2YQeDqGCP/xO9B+5gQNlyGZ9gSmFz+68NhYQ0vRekikpb9jNdy6ZZbfZDLp1w7dxqDIKfoyu7QO3Qr3E/9CpiucQif2p+oQOVOCdKEjvGYNkYQks0jVTYNRscgmcezpfLKhqWzAre5+JaMB0kRD5Nqadm2uXKZ4cNYStrpZ4xUrnMvAqjormxW2VJNx+0716Wc2Byhg8Nva+bsOkxp/GewBWHfNPtzQGMsL7oYZPtOd/LrmyYeu/M5Uz7/6QCv4N90P',
   }
+  sshkey { 'github-host-key':
+    name => 'github.com',
+    ensure => 'present',
+    key => 'AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==',
+    target => '/home/jenkins/.ssh/known_hosts',
+    type => 'ssh-rsa'
+  }
 
   exec { 'configure_sudo_tty_access':
     command => "sed -i 's|Defaults[ ]*requiretty|#Defaults    requiretty|g' /etc/sudoers",
