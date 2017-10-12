81fe52fb9dfa7b2c6bd15c7039fd47e79c364b9e
Jeremy Audet <ichimonji10@gmail.com>
2017-10-12 14:48:53 -0400

    Add Jenkins job to install and smash Pulp 3
    The new job is called `pulp-3-dev-{os}`. It can be run against Fedora 25
    and 26. It can't be run against RHEL, as neither it nor the Ansible
    playbooks it calls have logic for working with subscription manager.

---
+++ ci/jjb/scripts/pulp-smash-setup.sh
@@ -0,0 +1,33 @@
+mkdir -p ~/.virtualenvs/pulp-smash/
+python3 -m venv ~/.virtualenvs/pulp-smash
+source ~/.virtualenvs/pulp-smash/bin/activate
+pip install --upgrade pip
+pip install git+https://github.com/PulpQE/pulp-smash.git#egg=pulp-smash
+mkdir -p ~/.config/pulp_smash
+cat >~/.config/pulp_smash/settings.json <<EOF
+{
+    "pulp": {
+        "auth": ["admin", "admin"],
+        "version": "3"
+    },
+    "systems": [
+        {
+            "hostname": "$(hostname --long)",
+            "roles": {
+                "amqp broker": {"service": "rabbitmq"},
+                "api": {"scheme": "http"},
+                "mongod": {},
+                "pulp celerybeat": {},
+                "pulp cli": {},
+                "pulp resource manager": {},
+                "pulp workers": {},
+                "shell": {},
+                "squid": {}
+            }
+        }
+    ]
+}
+EOF
+pulp-smash settings path
+pulp-smash settings show
+pulp-smash settings validate
