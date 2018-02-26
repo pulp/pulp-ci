83213c83ce588c1f9c527b5020735db6a1928771
Jeremy Audet <jerebear@protonmail.com>
2018-02-26 16:40:09 -0500

    Test Pulp 3 development installs
    Add a new job: `pulp-3-devel-{os}`. This job installs a Pulp 3
    development environment on a host, and tests it with Pulp Smash.

---
+++ ci/jjb/scripts/pulp-3-devel-smasher.sh
@@ -0,0 +1,37 @@
+mkdir -p ~/.virtualenvs/pulp-smash/
+python3 -m venv ~/.virtualenvs/pulp-smash
+source ~/.virtualenvs/pulp-smash/bin/activate
+pip install --upgrade pip
+pip install git+https://github.com/PulpQE/pulp-smash.git#egg=pulp-smash
+mkdir -p ~/.config/pulp_smash
+cat >~/.config/pulp_smash/settings.json <<EOF
+{
+    "pulp": {
+        "auth": ["admin", "admin"],
+        "version": "3"
+    },
+    "systems": [
+        {
+            "hostname": "$(hostname --long)",
+            "roles": {
+                "amqp broker": {"service": "qpidd"},
+                "api": {"port": 8000, "scheme": "http"},
+                "mongod": {},
+                "pulp celerybeat": {},
+                "pulp cli": {},
+                "pulp resource manager": {},
+                "pulp workers": {},
+                "shell": {},
+                "squid": {}
+            }
+        }
+    ]
+}
+EOF
+pulp-smash settings path
+pulp-smash settings show
+pulp-smash settings validate
+# Use pytest instead of unittest for XML reports. :-(
+pip install pytest
+py.test -v --color=yes --junit-xml=junit-report.xml --pyargs pulp_smash.tests
+test -f junit-report.xml
