a3a662189b2b0ab6dcc14400b651765f3357bf51
ragabala <ragavendranb92@gmail.com>
2018-08-15 09:49:15 -0400

    Adding F28 python3 firewalld bindings
    This PR addresses two issues that are related to Fedora28.
    
    It makes sure Python3 interpreter is used for running Ansible in Fedora
    28. This was required since the python2-firewall packages are not used
    in F28.
    SELinux is set in the permissive mode in F28, Since the tasks that
    starts squid.service [1] fails in F28.
    
    Since the travis builds fails for testing jjb. Updating JJB in the
    travis jobs.
    
    [1] https://pulp.plan.io/issues/3926

---
+++ ci/jjb/scripts/pulp-install-f28.sh
@@ -0,0 +1,12 @@
+setenforce permissive
+sudo yum -y install ansible attr git libselinux-python
+echo 'localhost' > hosts
+source "${RHN_CREDENTIALS}"
+export ANSIBLE_CONFIG="${PWD}/ci/ansible/ansible.cfg"
+ansible-playbook --connection local -i hosts ci/ansible/pulp_server.yaml \
+    -e "pulp_build=${PULP_BUILD}" \
+    -e "pulp_version=${PULP_VERSION}" \
+    -e "rhn_password=${RHN_PASSWORD}" \
+    -e "rhn_pool=${RHN_POOL}" \
+    -e "rhn_username=${RHN_USERNAME}" \
+    -e "ansible_python_interpreter=/usr/bin/python3"
