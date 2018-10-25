dcaa22a3fa36da7d99a04d45e980760f7b8a39c3
kersom <kersom@gmail.com>
2018-10-25 16:32:10 -0400

    Remove Fedora from test matrix
    As discussed with the rest of the team, Fedora is being removed from the test
    matrix for Pulp 2.

--- ci/jjb/scripts/pulp-install-f28.sh
+++
@@ -1,8 +0,0 @@
-sudo dnf -y update
-sudo dnf -y install ansible attr git libselinux-python
-echo 'localhost' > hosts
-export ANSIBLE_CONFIG="${PWD}/ci/ansible/ansible.cfg"
-ansible-playbook --connection local -i hosts ci/ansible/pulp_server.yaml \
-    -e "pulp_build=${PULP_BUILD}" \
-    -e "pulp_version=${PULP_VERSION}" \
-    -e "ansible_python_interpreter=/usr/bin/python3"
