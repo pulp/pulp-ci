7eb17f2299b412bab82426bf79c588c9257c3ea4
kersom <kersom@gmail.com>
2018-12-05 09:42:34 -0500

    Remove CDN repos from upgrade jobs
    Remove CDN repos from upgrade jobs. Until CDN access is sorted out.

--- ci/jjb/scripts/post-upgrade.sh
+++ ci/jjb/scripts/post-upgrade.sh
@@ -16,9 +16,4 @@ pulp-admin rpm repo list --repo-id rpm-signed
 pulp-admin rpm repo list --repo-id srpm
 pulp-admin rpm repo list --repo-id sync-schedule
 
-if [[ -f "${CDN_CERTIFICATES}" ]]; then
-    pulp-admin rpm repo list --repo-id rhel6
-    pulp-admin rpm repo list --repo-id rhel7-rhn-tools
-fi
-
 pulp-admin logout
