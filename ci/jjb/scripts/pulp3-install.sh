85ab7f9374651289088e36468589b1a71e8959e2
Bruno Rocha <rochacbruno@gmail.com>
2019-02-05 20:31:31 -0200

    Firewall should be disable after pulp3 installation

--- ci/jjb/scripts/pulp3-install.sh
+++ ci/jjb/scripts/pulp3-install.sh
@@ -23,9 +23,9 @@ echo "Create hosts file."
 echo 'localhost' > hosts
 source "${RHN_CREDENTIALS}"
 
+echo "Starting Pulp 3 Installation."
+ansible-playbook --connection local -i hosts -u root install.yml -v -e pulp_pip_editable=no -e pulp_content_host="$(hostname --long):8080"
+
 echo "Disabling Firewall service to enable access to custom content ports"
 sudo systemctl disable firewalld
 sudo systemctl stop firewalld
-
-echo "Starting Pulp 3 Installation."
-ansible-playbook --connection local -i hosts -u root install.yml -v -e pulp_pip_editable=no -e pulp_content_host="$(hostname --long):8080"
