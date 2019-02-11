ee17e634f709b51b26d6dd360c46ed3c30b6a6bb
ragabala <ragavendranb92@gmail.com>
2019-02-11 13:38:14 -0500

    Adding python3 installation along with disk image builder patch
    The  current disk image builder is failing because of the issue with
    virtualenv in version (2.20.0). The current fix uses the patch for [disk
    image
    builder](https://launchpadlibrarian.net/355559006/diskimage-builder.patch)
    and also uses stable version of diskimage_builder util (2.17.0) .

--- ci/jjb/scripts/disk-image-builder.sh
+++ ci/jjb/scripts/disk-image-builder.sh
@@ -4,6 +4,6 @@ cat "${RHN_CREDENTIALS}" >> ./env_variables.sh
 cat "${Open_Stack_Credentials}" >> ./env_variables.sh
 rm -rf ~/.venvs/openstack/
 # Creating a python virtual env, since pypi packages are needed
-virtualenv ~/.venvs/openstack/
+python3 -m venv ~/.venvs/openstack/
 source ~/.venvs/openstack/bin/activate
 ./ci/open_stack_plugin/disk_image_builder/scripts/builder.sh all
