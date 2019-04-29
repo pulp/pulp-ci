c5bc5c60eb6fd3efeb85ddcd1dcbebd27b762a50
Brian Herring <bherring@redhat.com>
2019-04-29 14:14:28 -0400

    Removal of disk-image-builder
    Pulp QE is simplifying our image building process. The disk-image-builder
    project is being removed in favor of an ansible playbook, base_image_ci.yaml.
    
    The playbook, along with current documentation of manual procedures, will aid
    in the infrequent creation of new base images for Jenkins Openstack Plugin templates.
    
    For more backgroun information, search SATQE JIRA issues for the tag `infrastructure`.
    
    See:
    * https://github.com/pulp/pulp-ci/pull/667
    * https://projects.engineering.redhat.com/browse/SATQE-2740

--- ci/jjb/scripts/disk-image-builder.sh
+++
@@ -1,9 +0,0 @@
-touch ./env_variables.sh
-chmod +x ./env_variables.sh
-cat "${RHN_CREDENTIALS}" >> ./env_variables.sh
-cat "${Open_Stack_Credentials}" >> ./env_variables.sh
-rm -rf ~/.venvs/openstack/
-# Creating a python virtual env, since pypi packages are needed
-python3 -m venv ~/.venvs/openstack/
-source ~/.venvs/openstack/bin/activate
-./ci/open_stack_plugin/disk_image_builder/scripts/builder.sh all
