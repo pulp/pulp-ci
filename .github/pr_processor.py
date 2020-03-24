ac763741acef0cc3dad4bf9ee4cc726a254cb176
David Davis <daviddavis@redhat.com>
2020-03-24 08:43:50 -0400

    Add PR Processor job
    fixes #4365
    https://pulp.plan.io/issues/4365

---
+++ .github/pr_processor.py
@@ -0,0 +1,94 @@
+import os
+import re
+from datetime import datetime, timedelta
+
+from github import Github
+from github.GithubException import UnknownObjectException
+from redminelib import Redmine
+
+ORG = "pulp"
+REPOS = ["pulpcore", "pulp_file", "pulp_rpm"]
+SINCE = datetime.utcnow() - timedelta(hours=1)
+KEYWORDS = ["fixes", "closes", "re", "ref"]
+PR_STATUS = 3  # POST
+
+# redmine
+REDMINE_URL = "https://pulp.plan.io"
+REDMINE_KEY = os.environ["REDMINE_API_KEY"]
+
+# github
+GITHUB_USER = 'pulpbot'
+GITHUB_TOKEN = os.environ["GITHUB_API_TOKEN"]
+
+
+def user_comment(issue, user=GITHUB_USER):
+    """Find the first comment by a user for an issue."""
+    for comment in issue.get_comments():
+        if comment.user.login == user:
+            return comment
+
+
+if not REDMINE_KEY or not GITHUB_TOKEN:
+    print("Missing redmine and/or github api key.")
+    exit(0)
+
+redmine = Redmine(REDMINE_URL, key=REDMINE_KEY)
+g = Github(GITHUB_TOKEN)
+
+regex = r"(?:{keywords})[\s:]+#(\d+)".format(keywords=("|").join(KEYWORDS))
+pattern = re.compile(regex)
+
+for repo in REPOS:
+    grepo = g.get_repo(f"{ORG}/{repo}")
+    issues = grepo.get_issues(since=SINCE)
+
+    for issue in issues:
+        pr = issue.as_pull_request()
+
+        # check if we've seen this PR already
+        if user_comment(pr):
+            continue
+
+        r_issues = []
+        for commit in pr.get_commits():
+            message = commit.commit.message
+            r_issues.extend(pattern.findall(message))
+
+        # UPDATE THE ISSUE IN REDMINE
+
+        needs_cherry_pick = False
+
+        if not r_issues:
+            comment = (
+                "WARNING!!! This PR is not attached to an issue. In most cases this is not advisable. "
+                "Please see [our PR docs](http://docs.pulpproject.org/contributing/git.html#commit-message)"
+                " for more information about how to attach this PR to an issue."
+            )
+        else:
+            comment = ""
+            for issue_num in r_issues:
+                issue = redmine.issue.get(issue_num)
+
+                if issue.tracker.name == "Issue":
+                    needs_cherry_pick = True
+
+                if issue.status.id <= PR_STATUS:
+                    redmine.issue.update(issue_num, status_id=3, notes=f"PR: {pr.url}")
+                    comment += f"Attached issue: {issue.url}\n\n"
+                else:
+                    comment += f"Warning: Issue [#{issue.id}]({issue.url}) is not at NEW/ASSIGNED/POST.\n\n"
+                    redmine.issue.update(issue_num, notes=f"PR: {pr.url}")
+
+        grepo.get_issue(pr.number).create_comment(comment)
+
+        # ADD LABELS
+
+        if needs_cherry_pick:
+            try:
+                label = grepo.get_label("Needs Cherry Pick")
+            except UnknownObjectException:
+                print("No cherry pick label found.")
+                continue
+
+            labels = pr.labels + [label]
+            grepo.get_issue(pr.number).edit(labels=[l.name for l in labels])
